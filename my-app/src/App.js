1ca711c9d58b3e39cf26b8df39a02786a773ac43
Git-Fanfo <juanjosedoradom@gmail.com>
2022-06-06 19:16:12 -0500

    Router creado (?

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,22 @@
 import React from 'react'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom"
+
+import Test1 from './components/Test1';
+import Login from './pages/Login';
 
 const app = () => {
   return (
-    <div>
-        ESTO ES APP
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/test" element={<Test1 />} />   
+        <Route path="/" element={<Login />} />
+        <Route path="*" element={<Login />} />
+      </Routes>
+    </Router>
   )
 }
 
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,27 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from "react-router-dom";
-import Login from './pages/Login';
 
-import Test1 from './components/Test1';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        
-        <Route path="/" exact>
-          <App />
-        </Route>
-        <Route path="/test" element={<Test1 />} />    
-
-      </Routes>
-    </BrowserRouter>      
-  </React.StrictMode>
+  <App />
 );

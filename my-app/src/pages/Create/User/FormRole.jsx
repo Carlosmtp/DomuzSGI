911d58c8405c3a94f8ea2bf81f9c6e0effac2f11
Git-Fanfo <juanjosedoradom@gmail.com>
2022-06-15 16:37:24 -0500

    Pagination funcionando

--- my-app/src/pages/Create/User/FormRole.jsx
+++ my-app/src/pages/Create/User/FormRole.jsx
@@ -3,35 +3,43 @@ import { Avatar, Box, Divider, Grid, Pagination, Typography } from '@mui/materia
 import { deepPurple } from '@mui/material/colors'
 import React from 'react'
 
-const rol = 'El colaborador es el encargado de llenar las iniciativas para tener la información actualizada de los procesos, únicamente puede ver los procesos que se le han sido asignados.'
 
-const FormRole = () => {
+const FormRole = ({
+                    role,
+                    setRole,
+                    description,
+                    setDescription,
+                    descriptions,
+                    roles
+}) => {
 
-    const handleChange = (event) => {
-        console.log(event.target.textContent)
-
+    const handleChange = (event, page) => {
+        setRole(roles[page-1])
+        setDescription(descriptions[page-1])
   };
 
   return (
     <Box bgcolor='gray' p={4} sx={{ borderRadius: '16px' }}>
-        <Grid container spacing={1} xs={12}>
-            <Grid item xs={12} sm={6}>
+        <Grid container spacing={1} >
+            <Grid item xs={12} sm={6} align="center" justify="center">
                 <Avatar
-                alt="Colaborador"
+                alt={role}
                 sx={{ width: { xs: 78, sm: 128}, height: { xs: 78, sm: 128}, bgcolor:deepPurple[500] }}
+                p={2}
                 >
-                <Engineering sx={{ width: { xs: 56, sm: 80}, height: { xs: 56, sm: 80} }}/>
+                    <Engineering sx={{ width: { xs: 56, sm: 80}, height: { xs: 56, sm: 80} }}/>
                 </Avatar>
             </Grid>
             <Grid item xs={12} sm={6}>
                 <Divider sx={{ my: 1 }}>
                 <Typography variant='h5'>
-                    Colaborador
+                    {role}
                 </Typography>
                 </Divider>
-                {rol}            
-                <Pagination  count={3} onChange={handleChange} color="secondary" />
+                {description}            
+                
             </Grid>
+            <Pagination  count={roles.length} onChange={handleChange} color="secondary" />
         </Grid>
         </Box> 
   )

e0e59643184bea4f8953f73233ba7dd4c20263cc
Carlos Tovar <carlos.mauricio.tovar@correounivalle.edu.co>
2022-07-06 00:11:34 -0500

    función para obtener el último periodic_record de un indicador

--- api/routes/processes.js
+++ api/routes/processes.js
@@ -100,4 +100,16 @@ api.get("/get/periodic_records/", async (req, res) => {
   }
 })
 
+api.get("/get/last_record/", async (req, res) => {
+  const indicatorId = req.query.indicator_id;
+  const lastRecord = await prisma.periodic_records.findFirst({
+    where: {
+      indicatorId: parseInt(indicatorId),
+    },
+    orderBy: [
+      {record_date : 'desc'}
+    ]
+  })
+  res.json(lastRecord)
+})
 module.exports = api;

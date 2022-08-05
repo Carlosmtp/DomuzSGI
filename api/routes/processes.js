997d9cad07be323627351a51bcadfcb42e62e8d4
Alejandro Caicedo <alejandro.caicedo.caicedo@correounivalle.edu.co>
2022-08-05 13:15:35 -0500

    auto insert en process_report

--- api/routes/processes.js
+++ api/routes/processes.js
@@ -1,6 +1,4 @@
-const {
-  PrismaClient
-} = require("@prisma/client");
+const { PrismaClient } = require("@prisma/client");
 const express = require("express");
 const prisma = new PrismaClient();
 const api = express.Router();
@@ -12,7 +10,7 @@ api.post("/create/proccess", async (req, res) => {
       name: data.name,
       description: data.description,
       efficiency: data.efficiency,
-      goal : DataTransfer.goal,
+      goal: DataTransfer.goal,
       indicators: {
         createMany: {
           data: data.indicators,
@@ -27,10 +25,10 @@ api.post("/create/proccess", async (req, res) => {
 api.post("/update/proccess", async (req, res) => {
   const data = req.body;
   const process = await prisma.processes.update({
-    where : {
-      id : data.id
+    where: {
+      id: data.id,
     },
-    data: data
+    data: data,
   });
   res.json(process);
 });
@@ -58,13 +56,13 @@ api.post("/create/process/indicators", async (req, res) => {
 api.post("/update/process/indicator", async (req, res) => {
   const data = req.body;
   const indicator = await prisma.process_indicators.update({
-    where : {
-      id : data.id
+    where: {
+      id: data.id,
     },
     data: data,
   });
   res.json(indicator);
-})
+});
 
 api.get("/get/process/indicators", async (req, res) => {
   const indicators = await prisma.process_indicators.findMany();
@@ -74,17 +72,17 @@ api.get("/get/process/indicators", async (req, res) => {
 api.get("/get/periodicities", async (req, res) => {
   const periodicities = await prisma.periodicities.findMany();
   res.json(periodicities);
-})
+});
 
 api.delete("/delete/process/indicator", async (req, res) => {
   const id = parseInt(req.body.indicator_id);
   const del = await prisma.process_indicators.delete({
-    where : {
+    where: {
       id: id,
     },
-  })
-  res.json("Indicador Eliminado")
-})
+  });
+  res.json("Indicador Eliminado");
+});
 
 ///////////////////////// Periodic Records ///////////////////////
 api.post("/add/periodic_record", async (req, res) => {
@@ -100,46 +98,122 @@ api.post("/add/periodic_record", async (req, res) => {
       efficiency: data.archieved_value / data.expected_value,
     },
   });
-  res.json(record);
+
+  /* const id_p = await prisma.process_indicators.findUnique({
+    where: {
+      id: data.indicator_id,
+    },
+    select: {
+      processId: true,
+    },
+  });
+
+  const indics = await prisma.process_indicators.findMany({
+    where: {
+      processId: id_p.processId,
+    },
+    select: {
+      id: true,
+    },
+  });
+
+  let last = [];
+  for (let i = 0; i < indics.length; i++) {
+    last[i] = await prisma.periodic_records.findFirst({
+      where: {
+        indicatorId: indics[i].id,
+        record_date: {
+          lte: new Date(dateToSearch),
+        },
+      },
+      orderBy: [{ record_date: "desc" }],
+    });
+  }
+
+  let sum_1 = 0;
+  let sum_2 = 0;
+  last.forEach((e) => {
+    if (e != null) {
+      sum_1 += e.efficiency * e.weight;
+      sum_2 += e.goal * e.weight;
+    }
+  });
+  let processEfficency = sum_1 / sum_2;
+  //console.log(sum_1);
+  //console.log(sum_2);
+  //console.log(processEfficency);
+
+  const report = await prisma.process_reports.create({
+    data: {
+      processId: id_p.processId,
+      date: new Date(data.record_date),
+      efficiency: processEfficency,
+      goal: 0.5,
+    },
+  }); */
+
+  const report = await autoInsertReport(data.indicator_id, data.record_date);
+  res.json([record, report]);
+});
+
+api.post("/update/periodic_record", async (req, res) => {
+  const data = req.body;
+  const record = await prisma.periodic_records.update({
+    where: {
+      id: data.id,
+    },
+    data: {
+      indicatorId: data.indicator_id,
+      expected_value: data.expected_value,
+      archieved_value: data.archieved_value,
+      record_date: new Date(data.record_date),
+      goal: data.goal,
+      weight: data.weight,
+      efficiency: data.archieved_value / data.expected_value,
+    },
+  });
+
+  const report = await autoInsertReport(data.indicator_id, data.record_date);
+  res.json(report);
 });
 
 api.get("/get/periodic_records/", async (req, res) => {
   const year = req.query.year;
   const month = req.query.month;
-  const indicator = req.query.indicator
+  const indicator = req.query.indicator;
   try {
     //in this json are the clauses indicated in the url
     var clauses = {
-      where: {}
+      where: {},
     };
     //indicator clause
     if (indicator !== undefined) {
       clauses.where.indicatorId = parseInt(indicator);
     }
     //date clause when only the year was entered
-    if (year !== undefined & month == undefined) {
+    if ((year !== undefined) & (month == undefined)) {
       clauses.where.record_date = {
         gte: new Date(year + "-jan-01"),
-        lte: new Date(year + "-dec-31")
+        lte: new Date(year + "-dec-31"),
       };
       //date clause when the year and month were entered
-    } else if (year !== undefined & month !== undefined) {
+    } else if ((year !== undefined) & (month !== undefined)) {
       clauses.where.record_date = {
         gte: new Date(year + month + "-01"),
-        lte: new Date(year + month + "-31")
+        lte: new Date(year + month + "-31"),
       };
       //error when the month was entered but not the year
-    } else if (year == undefined & month !== undefined) {
-      throw new Error("si desea filtrar por mes, debe ingresar el año")
+    } else if ((year == undefined) & (month !== undefined)) {
+      throw new Error("si desea filtrar por mes, debe ingresar el año");
     }
-    const periodic_records = await prisma.periodic_records.findMany(clauses)
+    const periodic_records = await prisma.periodic_records.findMany(clauses);
     res.json(periodic_records);
   } catch (e) {
     res.json({
       error: e.message,
     });
   }
-})
+});
 
 api.get("/get/last_record/", async (req, res) => {
   const indicatorId = req.query.indicator_id;
@@ -147,12 +221,10 @@ api.get("/get/last_record/", async (req, res) => {
     where: {
       indicatorId: parseInt(indicatorId),
     },
-    orderBy: [
-      {record_date : 'desc'}
-    ]
-  })
-  res.json(lastRecord)
-})
+    orderBy: [{ record_date: "desc" }],
+  });
+  res.json(lastRecord);
+});
 
 ///////////////////////// Reports ///////////////////////
 api.get("/get/last_report/", async (req, res) => {
@@ -161,11 +233,69 @@ api.get("/get/last_report/", async (req, res) => {
     where: {
       processId: parseInt(processId),
     },
-    orderBy: [
-      {date : 'desc'}
-    ]
-  })
-  res.json(lastReport)
-})
+    orderBy: [{ date: "desc" }],
+  });
+  res.json(lastReport);
+});
+
+async function autoInsertReport(idIndicator, date) {
+  const dateToRecord = date.split("-");
+  const dateToSearch = dateToRecord[0] + "-" + dateToRecord[1] + "-31";
+
+  const id_p = await prisma.process_indicators.findUnique({
+    where: {
+      id: idIndicator,
+    },
+    select: {
+      processId: true,
+    },
+  });
+
+  const indics = await prisma.process_indicators.findMany({
+    where: {
+      processId: id_p.processId,
+    },
+    select: {
+      id: true,
+    },
+  });
+
+  let last = [];
+  for (let i = 0; i < indics.length; i++) {
+    last[i] = await prisma.periodic_records.findFirst({
+      where: {
+        indicatorId: indics[i].id,
+        record_date: {
+          lte: new Date(dateToSearch),
+        },
+      },
+      orderBy: [{ record_date: "desc" }],
+    });
+  }
+
+  let sum_1 = 0;
+  let sum_2 = 0;
+  last.forEach((e) => {
+    if (e != null) {
+      sum_1 += e.efficiency * e.weight;
+      sum_2 += e.goal * e.weight;
+    }
+  });
+  let processEfficency = sum_1 / sum_2;
+  //console.log(sum_1);
+  //console.log(sum_2);
+  //console.log(processEfficency);
+
+  const report = await prisma.process_reports.create({
+    data: {
+      processId: id_p.processId,
+      date: new Date(date),
+      efficiency: processEfficency,
+      goal: 0.5,
+    },
+  });
+
+  return report;
+}
 
 module.exports = api;
